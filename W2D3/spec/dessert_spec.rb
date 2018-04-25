b536abbda3af9fbbb920e8354b847d4ac218e86f
alexg622 <alex.gm62288@gmail.com>
2018-04-24 20:45:33 -0700

    Pass specs

---
+++ W2D3/spec/dessert_spec.rb
@@ -0,0 +1,76 @@
+require 'rspec'
+require 'dessert'
+
+=begin
+Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
+=end
+
+describe Dessert do
+  let(:chef) { double("chef") }
+  let(:dessert) { Dessert.new("good", 10, chef)}
+
+  describe "#initialize" do
+    it "sets a type" do
+      expect(dessert.type).to eq("good")
+    end
+
+    it "sets a quantity" do
+      expect(dessert.quantity).to eq(10)
+    end
+
+    it "starts ingredients as an empty array" do
+      expect(dessert.ingredients).to be_empty
+    end
+
+    it "raises an argument error when given a non-integer quantity" do
+      expect{Dessert.new("bad", "bad", chef)}.to raise_error(ArgumentError)
+    end
+  end
+
+  describe "#add_ingredient" do
+    it "adds an ingredient to the ingredients array" do
+      dessert.add_ingredient("eggs")
+      expect(dessert.ingredients).to include("eggs")
+    end
+
+  end
+
+  describe "#mix!" do
+    it "shuffles the ingredient array" do
+      dessert.add_ingredient("milk")
+      dessert.add_ingredient("butter")
+      dessert.add_ingredient("honey")
+      dessert.add_ingredient("spice")
+      dessert.add_ingredient("pepper")
+      ingredients = dessert.ingredients.dup
+      dessert.mix!
+      expect(dessert.ingredients).to_not eq(ingredients)
+    end
+  end
+
+  describe "#eat" do
+    it "subtracts an amount from the quantity" do
+      quantity = dessert.quantity
+      dessert.eat(5)
+      expect(dessert.quantity).to be < quantity
+    end
+
+    it "raises an error if the amount is greater than the quantity" do
+      expect{dessert.eat(20)}.to raise_error("not enough left!")
+    end
+  end
+
+  describe "#serve" do
+    it "contains the titleized version of the chef's name" do
+      allow(chef).to receive(:titleize).and_return("Chef chef the Great Baker")
+      expect(dessert.serve).to eq("Chef chef the Great Baker has made 10 goods!")
+    end
+  end
+
+  describe "#make_more" do
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      expect(chef).to receive(:bake).with(dessert)
+      dessert.make_more
+    end
+  end
+end
