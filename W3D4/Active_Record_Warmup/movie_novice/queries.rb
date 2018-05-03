1155e376633b8f76e6cdec38e8379c3df59a56f0
alexg622 <alex.gm62288@gmail.com>
2018-05-02 22:38:20 -0700

    Fix indentation on Active Record Warm Up

--- W3D4/Active_Record_Warmup/movie_novice/queries.rb
+++ W3D4/Active_Record_Warmup/movie_novice/queries.rb
@@ -70,5 +70,5 @@ def uma_movies
   # display the id, title, and year of movies Uma Thurman has acted in
   # order them by ascending year
   # hint: use 'select', 'joins', 'where', and 'order'
-Movie.select(:id, :title, :yr).joins(:actors).where('name = \'Uma Thurman\'').order('yr ASC')
+  Movie.select(:id, :title, :yr).joins(:actors).where('name = \'Uma Thurman\'').order('yr ASC')
 end
